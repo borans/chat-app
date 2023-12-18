37e2f918cc86a2bb1a6c781c976849758e02395b
borans <boransezen1@gmail.com>
2023-12-18 11:33:12 +0300

    User variables defined, Message entity created

--- src/main/java/com/example/chatapp/models/User.java
+++ src/main/java/com/example/chatapp/models/User.java
@@ -1,4 +1,31 @@
 package com.example.chatapp.models;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    private Long userId;
+
+    private String userName;
+
+    private List<Message> messageList;
+
+
+
+
+
 }
