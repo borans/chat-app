54e49455ef7fe3945db868cc3e4e23262bd0ade9
borans <boransezen1@gmail.com>
2023-12-23 11:01:49 +0300

    Message and Group entities created.

--- src/main/java/com/example/chatapp/models/User.java
+++ src/main/java/com/example/chatapp/models/User.java
@@ -1,20 +1,17 @@
 package com.example.chatapp.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "user")
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class User {
 
     @Id
@@ -22,9 +19,21 @@ public class User {
 
     private String userName;
 
-    private List<Message> messageList;
+    /*
+     @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    // we need to configure the many-to-many relationship so that it can be reflected
+    // to the database
+    @JoinTable(
+            name = "user_group",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "group_id")
+    )
+    @JsonIgnore
+    private Set<Group> groups;
 
 
+
+     */
 
 
 
